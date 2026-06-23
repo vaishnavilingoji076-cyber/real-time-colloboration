8801e8388434195bf1177240c62d7510f8739d4e
Your Name <vaishnavilingoji076@gmail.com>
2026-06-23 12:50:31 +0530

    added codeTemplates

---
+++ frontend/src/components/codeTemplates.js
@@ -0,0 +1,23 @@
+export const codeTemplates={
+    javascript:
+    `console.log("hello world");`,
+
+    python:
+    `print("hello world)`,
+
+    java:
+    `public class Main{
+    public static void main(String[]args){
+    system.out.printIn("hello vaish..);
+    }}`,
+
+    cpp:
+    `#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World";
+  return 0;
+}`
+};
+    
